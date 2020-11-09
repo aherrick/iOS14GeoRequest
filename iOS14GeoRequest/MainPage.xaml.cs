025a6245a97708288c26458f5ecaa5658ce24e7e
Andrew Herrick <aj@andrewherrick.com>
2020-11-09 09:02:48 -0700

    Initial commit

---
+++ iOS14GeoRequest/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace iOS14GeoRequest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium)
+            {
+                Timeout = TimeSpan.FromSeconds(30)
+            };
+
+            var location = await Geolocation.GetLocationAsync(request);
+
+            await DisplayAlert("Lat Lng", $"{location.Latitude} {location.Longitude}", "Ok");
+        }
+    }
+}
